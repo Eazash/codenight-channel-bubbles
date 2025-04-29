3c910ea1ae1813fd64ccc60f73f402b28f90c8bd
Ezira Ashenafi <eazash22@gmail.com>
2025-04-29 12:49:34 +0300

    feat: ensure channel names are unique

---
+++ server/database/migrations/0002_majestic_marvel_apes.sql
@@ -0,0 +1,2 @@
+CREATE UNIQUE INDEX `channels_chat_id_unique` ON `channels` (`chat_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `channels_channel_username_unique` ON `channels` (`channel_username`);
